6e60e82cf787246021bef963476d899dbb8f8fc2
oguz.olke <oguz.olke@hepsiburada.com>
2022-06-12 19:12:17 +0300

    Create base structure

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser')
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json())
+app.use(
+   express.urlencoded({
+      extended: 'true',
+   })
+);
 
 
 
---
+++ routes/feedback-routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/', feedbackController.postFeedback);
+
+
+module.exports = router;
